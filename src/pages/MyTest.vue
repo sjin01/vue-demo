f46077c12e3a2a5bcbed3fafdaf1b3c8a1c7c95c
sjin01 <xiao_wen202129@163.com>
2017-01-05 14:35:58 +0800

    1

--- src/pages/MyTest.vue
+++ src/pages/MyTest.vue
@@ -29,7 +29,7 @@
         <input type="text" v-model="addcar.price">
       </div>
       <button type="button" @click="addCar">保存</button>
-      <button type="button" @click="getCustomers">从远程取</button>
+      <button type="button" @click="loadData">从远程取</button>
     </div>
   </main-layout>
 </template>
@@ -38,13 +38,38 @@
   import MainLayout from '../layouts/Main.vue'
 
   export default {
-    ready() {
-        console.log(1);
+    mounted: function () {
+        this.loadData();
     },
     data(){
       return {
         addcar:{},
-        cars:[]
+        cars:[{
+          id:1,
+          name:'x1',
+          trademark:'宝马',
+          price:'300000'
+        },{
+          id:2,
+          name:'x2',
+          trademark:'宝马',
+          price:'400000'
+        },{
+          id:3,
+          name:'x3',
+          trademark:'宝马',
+          price:'500000'
+        },{
+          id:4,
+          name:'x4',
+          trademark:'宝马',
+          price:'800000'
+        },{
+          id:5,
+          name:'x5',
+          trademark:'宝马',
+          price:'10000000'
+        }]
       }
     },
     methods:{
@@ -56,25 +81,32 @@
       delBook(index){
         this.cars.splice(index, 1);
       },
+
       loadData (){
-        this.$http.jsonp('cars.json').then( function(response) {
+        this.$http.get('src/json/cars.json').then( function(response) {
           console.log(response);
+          // this.$set('cars', response.data);
+          this.cars = this.cars.concat(response.data);
         }).catch(function(response) {
           console.log(response);
         })
       },
+
+      /*// $resource 另一个从远程取数据方法
       getCustomers: function() {
-        var resource = this.$resource('cars.json');
+        var resource = this.$resource('src/json/cars.json');
         var vm = this;
 
         resource.get()
           .then((response) => {
             console.log(response.data);
+            this.cars = this.cars.concat(response.data);
+//            vm.$set('cars', response.data)
           })
           .catch(function(response) {
             console.log(response)
           })
-      }
+      }*/
     },
     components: {
       MainLayout
